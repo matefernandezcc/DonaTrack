fde63c7e20f6c96e60d2f8bb1c3ea0ea8098e88c
matefernandezcc <matefernandezc@gmail.com>
2026-07-04 15:29:50 -0300

    Merge hacia Dev

--- donatrack-logistica/src/main/java/com/donatrack/logistica/application/ports/out/CamionRepositoryPort.java
+++ donatrack-logistica/src/main/java/com/donatrack/logistica/application/ports/out/CamionRepositoryPort.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 
 public interface CamionRepositoryPort {
     void guardar(Camion camion);
+
     Optional<Camion> buscarPorPatente(String patente);
+
     List<Camion> obtenerTodos();
+
     void eliminar(String patente);
 }
