23660db68a29f60ad2b168d2f6ba169fc1f8da65
matefernandezcc <matefernandezc@gmail.com>
2026-08-28 12:00:50 -0300

    entrega tp4 enunciado

--- donatrack-server/src/test/java/com/donatrack/DonatrackApplicationTests.java
+++ donatrack-server/src/test/java/com/donatrack/DonatrackApplicationTests.java
@@ -1,10 +1,16 @@
 package com.donatrack;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class DonatrackTests {
+
+  // Mock del ConnectionFactory para evitar que RabbitMQ intente conectarse a un broker real
+  @MockitoBean
+  private ConnectionFactory connectionFactory;
 
   @Test
   void contextLoads() {
